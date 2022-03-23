76981f04af5feb5ae4be3786720c7e81f7eeb1c0
Nonso <nwabuezesc@gmail.com>
2022-03-23 19:21:18 +0100

    installing module for sending mail

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser');
 const bodyParser =  require('body-parser') 
-  const {User,Image,Member} =require('./models/user')
+const {User,Image,Member} =require('./models/user')
 const Authrouters = require('./routes/authrotes');
 
 //set up the express function
