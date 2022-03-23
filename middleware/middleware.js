5d31bdfc9b844f8ab50c3c7ff6e7896bf786c33d
Nonso <nwabuezesc@gmail.com>
2022-03-23 07:36:53 +0100

    fetching stored data in database

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -8,10 +8,8 @@ const requireAuth = (req, res, next) =>{
     if(token){
         jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken)=>{
             if(err){
-                console.log(err.message)
                 res.redirect('/login')
             }else{
-                console.log(decodedToken)
                 next()
             }
         })
@@ -27,11 +25,9 @@ const checkUser =  (req, res, next) =>{
     if(token){
         jwt.verify(token, process.env.JWT_SECRET, async(err, decodedToken)=>{
             if(err){
-                console.log(err.message);
                 res.locals.user = null;
                 next();
             }else{
-                console.log(decodedToken)
                 let user = await User.findById(decodedToken.id)
                 res.locals.user = user;
                 next()
