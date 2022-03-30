2d70753d11218286c76914aecda25b372149d20a
Nonso <nwabuezesc@gmail.com>
2022-03-30 19:55:01 +0100

    setting up more functionality

--- public/js/script.js
+++ public/js/script.js
@@ -16,3 +16,15 @@ function playVideo(file){
     myVideo.src = file
     videoPlayer.style.display = 'block'
 }
+
+
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
